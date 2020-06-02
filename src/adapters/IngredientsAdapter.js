f2d91c2c5520c6bc68821868f80c9b420a4986e9
Felicia_Smith <Smithfn1012@gmail.com>
2020-06-02 18:10:58 -0500

    first commit

---
+++ src/adapters/IngredientsAdapter.js
@@ -0,0 +1,9 @@
+class IngredientAdapter {
+    contructor() {
+        this.baseURL = "http://localhost:3000/ingredients"
+    }
+
+    getIngredients() {
+        return fetch(this.baseURL).then(response => response.json()).then(json => (json.data))
+    }
+}
